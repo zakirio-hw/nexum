2988928470c1ae01243925cc5dc07705fceee1aa
scuttyrht <scutty.rht@gmail.com>
2023-07-14 15:16:53 +0700

    Footer fix #1

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className='bg-green-500 neon-green text-white'>
         <div className='md:flex md:justify-between md:items-center sm:px-12 px-4 py-7'>
             <div><h1 className='text-5xl'>©</h1></div>
-            <div style={{marginRight:'84%'}}>
+            <div style={{marginRight:'80%'}}>
                 <p>
                     Adimas Farhan Putranto
                     <br></br>
